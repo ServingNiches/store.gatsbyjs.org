a12b27dfa03cfe654b1517cbf6912d3f8ad164ef
rtre84 <rtre84@users.noreply.github.com>
2022-04-22 21:26:32 +0530

    Added tracking code for Gatsby GA Suite

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,8 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'UA-151436119-1' // Google Analytics / GA
+          'UA-151436119-1', // Google Analytics / GA
+          'UA-151436119-2'
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
