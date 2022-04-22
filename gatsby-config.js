203d929e2e089595193b2fd5b5f95c48829d220b
rtre84 <rtre84@users.noreply.github.com>
2022-04-22 21:57:59 +0530

    Added new GA type account for tracking.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,8 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           'UA-151436119-1', // Google Analytics / GA
-          'UA-151436119-2'
+          'UA-151436119-2',
+          'G-Y1GXHFGFM1'
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
